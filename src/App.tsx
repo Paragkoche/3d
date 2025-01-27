b763bb5221d1aa3eb261b1740bfa3fb172a65893
paragkoche <paragkoche0@gmail.com>
2025-01-27 17:51:00 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+import "./App.css";
+import { useState } from "react";
+function Character({ fabric }: { fabric: string }) {
+  const { scene } = useGLTF("/model.glb");
+  const fabricTexture = useTexture(fabric);
+  scene.traverse((child: any) => {
+    if (child.isMesh) {
+      if (child.name == "Fabric") {
+        child.material.map = fabricTexture;
+        child.material.needsUpdate = true;
+      }
+    }
+  });
+  return <primitive object={scene} scale={1.5} />;
+}
+
+function App() {
+  const [fabric, setFabric] = useState("/EG-14.jpg");
+  return (
+    <div className="body">
+      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <Character fabric={fabric} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          dampingFactor={0.1}
+        />
+      </Canvas>
+      <div className="controller">
+        <div>
+          <div>
+            <button onClick={() => setFabric("/EG-14.jpg")}>
+              <img src="/EG-14.jpg" />
+            </button>
+            <button onClick={() => setFabric("/EG-15.jpg")}>
+              <img src="/EG-15.jpg" />
+            </button>
+            <button onClick={() => setFabric("/EG-16.jpg")}>
+              <img src="/EG-16.jpg" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
